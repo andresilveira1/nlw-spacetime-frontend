6310cc9e9cfcde8d17e3c8b38b9947584df4ed24
André Silveira <andre.silveira1silva@gmail.com>
2023-06-03 19:14:24 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com', '192.168.15.2'],
+  },
+}
 
 module.exports = nextConfig
